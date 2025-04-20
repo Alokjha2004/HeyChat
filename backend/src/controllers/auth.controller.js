1b8f3846999c887cd0b9ae43fa63826aab14b450
Alokjha2004 <arjha2004@gmail.com>
2025-04-21 03:19:08 +0530

    add auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 
@@ -5,6 +6,10 @@ import bcrypt from "bcryptjs"
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
     try {
+        // check if all fields are present
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "Please fill all fields" });
+        }
         if (password.length < 6) {
             return res.status(400).json({ message: "Password must be at least 6 characters" });
         }
@@ -25,11 +30,23 @@ export const signup = async (req, res) => {
 
         if (newUser) {
             //generate jwt token here
-        } else {
-            return res.status(400).json({message: "Invalid user data"});
-        }
-    } catch (error) {
+            generateToken(newUser._id, res)
+            await newUser.save();
 
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            })
+
+        } else {
+            return res.status(400).json({ message: "Invalid user data" });
+        }
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal server error" });
     }
 };
 
