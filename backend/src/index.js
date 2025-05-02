dd2f954e39c3bb6d8f7c019d93233d4e6e58c202
Alokjha2004 <arjha2004@gmail.com>
2025-05-02 21:47:53 +0530

    added cors to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
-import express from "express"; 
+import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser"; 
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js"; // Importing the auth routes
 import messageRoutes from "./routes/messageRoutes.js"; // Importing the auth routes
@@ -13,13 +14,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        Credential: true
+    })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(PORT,()=> {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`http://localhost:${PORT}`);
     connectDB();
-    
+
 });
