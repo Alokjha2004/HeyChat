acce53f07143d11eb87bf193032f53893294b212
Alokjha2004 <arjha2004@gmail.com>
2025-05-12 01:37:44 +0530

    change environment variable

--- api/src/lib/socket.js
+++ api/src/lib/socket.js
@@ -1,13 +1,14 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import { p } from "framer-motion/client";
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL],
   },
 });
 
