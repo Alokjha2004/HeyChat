2426360938022a998b6f3b33387cb9d20184ce64
Alokjha2004 <arjha2004@gmail.com>
2025-05-11 14:46:02 +0530

    add functionality to change theme

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
-  // headers: {
-  //   'Content-Type': 'application/json',
-  // },
 });
